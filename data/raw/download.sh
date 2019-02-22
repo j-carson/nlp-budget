445eb2bce370ea17d590c30b4a0e585d1f2d1ae2
j-carson <44308120+j-carson@users.noreply.github.com>
2019-02-21 21:40:41 -0800

    add pdf for reference

--- data/raw/download.sh
+++ data/raw/download.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wget  'https://www.congress.gov/116/bills/hjres31/BILLS-116hjres31enr.xml'  > hjres31.xml
+wget 'https://www.congress.gov/116/bills/hjres31/BILLS-116hjres31enr.xml'
+wget 'https://www.congress.gov/116/bills/hjres31/BILLS-116hjres31enr.pdf'
+
