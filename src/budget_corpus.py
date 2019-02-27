15a96bf8b37c25a928cff5f6bab96d17ce2db010
j-carson <44308120+j-carson@users.noreply.github.com>
2019-02-26 21:49:50 -0800

    consistent preprocessing

--- src/budget_corpus.py
+++ src/budget_corpus.py
@@ -5,7 +5,7 @@
 # 
 # To make sure all models are preprocessing the data in a similar way
 
-# In[24]:
+# In[97]:
 
 
 import pandas as pd
@@ -18,70 +18,68 @@ import pickle
 from pathlib import Path
 
 
-# In[25]:
+# In[98]:
 
 
 import gensim
 from gensim.utils import simple_preprocess
-from gensim.parsing.porter import PorterStemmer
+from gensim.utils import lemmatize
+from gensim.parsing.preprocessing import STOPWORDS as gs_stopwords
 
 
-# In[26]:
+# In[99]:
 
 
 docdir = Path('../data/docs')
 datadir = Path('../data')
 
 
-# In[27]:
+# In[108]:
 
 
 def read_raw_corpus():
     raw_corpus = []
 
     files = list(docdir.glob('*.body'))
-    header_files = [ (str(f)).replace('.body', '.heading') for f in files ]
     
-    for body, header in zip(files, header_files):
-        with open(header, 'r') as fp:
-            head_dict = json.load(fp)
-            fp.close()
-            
-        doc = ''
-        for key in ['division', 'title', 'major', 'inter', 'small']:
-            if head_dict[key] != '':
-                doc += head_dict[key] + '\n'
-        
+    for body in files:
         with open(body, 'r') as fp:
-            doc = '\n'.join([doc, fp.read()])
+            doc = fp.read()
             fp.close()
         raw_corpus.append(doc)
 
     return raw_corpus
 
 
-# In[28]:
+# In[109]:
 
 
 def tokenize_raw_budget(raw_corpus):
     corpus = []
+    stopwords =         """for the this that which such than with within without after from 
+        and but use each more less unless any law carry out 
+        has have are will shall may were been who its into subsection amount state grants 
+        fund funds funded costs expenses expended expenditure purchase account administration administrative
+        budget purposes during  united states national general government
+        house senate congress president office regulations act title code 
+        specified provided available further including herein
+        enactment program programs services operation operations activities activity
+        appropriation appropriations appropriated privision provisions agency agencies""".split()
     
-    # tla = three (or more) all-capitialized acronymns
+    
+    # tla = three (or more) letter all-capitialized acronymns
     tla = re.compile(r'[A-Z]{3,}\S*?\b')
-    p = PorterStemmer()
     
     for doc in raw_corpus:
-        # some headings are in all caps - don't mix those up with acronyms
-        headings, body =  doc.split('\n\n')
-        body = headings + ' ' + re.sub(tla, 'tla', body)
-        preproc = simple_preprocess(body, min_len=3)
-        stemmed = [ p.stem(w) for w in preproc ]
-        corpus.append(stemmed)
+        body = re.sub(tla, '', doc)
+        tokens = simple_preprocess(body, min_len=3)
+        tokens = [ t for t in tokens if t not in stopwords ]
+        corpus.append(tokens)
         
     return corpus
 
 
-# In[29]:
+# In[110]:
 
 
 def read_documents():
@@ -96,5 +94,4 @@ def read_documents():
         with open(doc_pickle, 'wb') as fp:
             pickle.dump(corpus, fp)
     return corpus
-
 
